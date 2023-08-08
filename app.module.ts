84688186e04d2d0194cf0103138ac7e5e8a9fcff
patilnilesh91 <136429206+patilnilesh91@users.noreply.github.com>
2023-08-09 01:38:03 +0530

    Add files via upload

---
+++ app.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { HttpClientModule } from '@angular/common/http';
+import { AppComponent } from './app.component';
+import { ProductcrudComponent } from './productcrud/productcrud.component';
+import { FormsModule } from '@angular/forms';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    ProductcrudComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpClientModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
